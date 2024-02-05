27076fe43489aed0f4056532bbea06208ecebbce
Agustin Bustos Piasentini bustos-agustin@hotmail.com <63634574+abustosp@users.noreply.github.com>
2024-02-05 18:19:11 -0300

    Update path to Python executable in Consolidar 3.0.bat

--- Consolidar 3.0.bat
+++ Consolidar 3.0.bat
@@ -1,3 +1,3 @@
 @echo off
-"F:\Proyectos Python\BIN_EV\Scripts\python.exe" "F:\Proyectos Python\Scripts\Consolidar MC\Consolidador 3.0 CTK GUI.py"
-exit
+"F:\Proyectos Python\Scripts\Consolidar MC\.ENV\Scripts\python.exe" "F:\Proyectos Python\Scripts\Consolidar MC\Consolidador 3.0 GUI.py"
+pause
